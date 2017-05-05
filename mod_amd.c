4eecf0813a4a9f28c322e4bafc9306dff475b832
Admin <srgdemon@gmail.com>
2017-05-05 14:10:42 +0300

    Added variable [amd_result_epoch] on application results

--- mod_amd.c
+++ mod_amd.c
@@ -505,6 +505,8 @@ SWITCH_STANDARD_APP(amd_start_function)
 		}
 	}
 
+	switch_channel_set_variable(channel, "amd_result_epoch", switch_mprintf( "%" SWITCH_TIME_T_FMT, switch_micro_time_now( )));
+	
 	switch_core_session_reset(session, SWITCH_FALSE, SWITCH_TRUE);
 	switch_core_codec_destroy(&raw_codec);
 }
